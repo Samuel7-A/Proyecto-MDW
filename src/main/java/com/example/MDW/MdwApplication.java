b278250964fe389a94112bb50c4e22d974ad2174
Samuel7-A <samuelmassawatorresayala@gmail.com>
2025-09-23 01:07:52 -0500

    Proyecto migrado a Springboot

---
+++ src/main/java/com/example/MDW/MdwApplication.java
@@ -0,0 +1,13 @@
+package com.example.MDW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MDW/MdwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MDW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
