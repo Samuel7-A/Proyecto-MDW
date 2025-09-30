9b058d0925d33ce60905383322cf279ffdd46e86
Samuel7-A <samuelmassawatorresayala@gmail.com>
2025-09-30 01:50:28 -0500

    proyecto actualizado navbar lateral, nombre de usuario visible en cursos

--- src/main/java/com/example/MDW/controller/HomeController.java
+++ src/main/java/com/example/MDW/controller/HomeController.java
@@ -4,7 +4,6 @@ package com.example.MDW.controller;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.MDW.model.Usuario;
