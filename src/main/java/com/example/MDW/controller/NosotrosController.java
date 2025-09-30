697028c4259f8bfbcf5d0afef94e07814f3974ca
Samuel7-A <samuelmassawatorresayala@gmail.com>
2025-09-30 17:12:19 -0500

    Correción vista de imagenes de Nosotros.html

--- src/main/java/com/example/MDW/controller/NosotrosController.java
+++ src/main/java/com/example/MDW/controller/NosotrosController.java
@@ -1,11 +1,11 @@
-package com.example.MDW.model;
+package com.example.MDW.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Nosotros {
+public class NosotrosController {
 
         @GetMapping("/Nosotros")
         public String mostrarNosotros(Model model) {
@@ -16,9 +16,12 @@ public class Nosotros {
                 model.addAttribute("vision", "Ser reconocidos como la academia virtual líder en Latinoamérica...");
 
                 // Imágenes (rutas dentro de /static/img/nosotros/)
-                model.addAttribute("imgResena", "/img/nosotros/historia.jpg");
-                model.addAttribute("imgMision", "/img/nosotros/mision.jpg");
-                model.addAttribute("imgVision", "/img/nosotros/vision.jpg");
+                model.addAttribute("imgResena", "/img/historia.jpg");
+                model.addAttribute("imgMision", "/img/mision.jpg");
+                model.addAttribute("imgVision", "/img/vision.jpg");
+                model.addAttribute("imgCarrusel1", "/img/conferencia.jpg");
+                model.addAttribute("imgCarrusel2", "/img/feria_digital.jpg");
+                model.addAttribute("imgCarrusel3", "/img/taller_practico.jpg");
 
                 return "Nosotros"; // se conecta con templates/Nosotros.html
         }
