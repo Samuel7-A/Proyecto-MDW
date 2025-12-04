3e27ca16f0805fe6bec0ea8d856f62d1a341b095
Growthen <arielandrerevilla@gmail.com>
2025-12-03 21:43:57 -0500

    Modal Contacto Encontrado xd

--- src/main/java/com/example/MDW/config/securityConfig.java
+++ src/main/java/com/example/MDW/config/securityConfig.java
@@ -56,6 +56,7 @@ public class securityConfig {
                                 "/login",
                                 "/register",
                                 "/Nosotros",
+                                "/contacto",
                                 "/niveles",
                                 "/cursos",
                                 "/carrito",
